7424134bf0de4a7a6da52a311f7281038765f478
agaziiantc60 <131266168+bread1337j@users.noreply.github.com>
2026-01-07 15:22:19 -0500

    removed consts

--- src/consts.c
+++
@@ -1,8 +0,0 @@
-#define DEFINE_GLOBALS 
-#include "types.h" 
-
-#ifndef NDEBUG 
-const u8 debugLogs = 0;
-#else 
-const u8 debugLogs = 1; 
-#endif
--- src/main.c
+++ src/main.c
@@ -4,6 +4,16 @@
 #include "ascii_image.h"
 
 int main(){
-	loadRawImage("assets/testsprite.txt");
+	rawImage* img = loadRawImage("assets/testsprite.txt");
+	for(int y=0; y<img->size_y; y++){
+		for(int x=0; x<img->size_x; x++){
+			pixel* p = &img->map[y * img->size_x + x];
+			printf("\033[48;2;%hhu;%hhu;%hhum%c\033[0m", p->r, p->g, p->b, p->c); 
+			//printf("\033[48;2;%hhu;%hhu;%hhum%c\033[0m", p->r, p->g, p->b, p->c); 
+		}
+		printf("\n");
+	}
+
+
 	return 0;
 }
