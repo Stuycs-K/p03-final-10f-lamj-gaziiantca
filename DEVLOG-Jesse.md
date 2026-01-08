c8d6dd9ae7b891a512ab6b3bf6335656cb1412a5
Retrialed <jessel40@nycstudents.net>
2026-01-08 06:47:09 -0500

    Added function to test frame timer and budget wasd inputs for changing player status

--- DEVLOG-Jesse.md
+++ DEVLOG-Jesse.md
@@ -1,20 +1,26 @@
 # Dev Log: Jesse Lam
 
 ### 2026-01-06 - Backseating the Proposal.md
-Added base file structure
+- Added base file structure
 
-Criticized the proposal
+- Criticized the proposal
 
-Spent 5 minutes adding to the DEVLOG
+- Spent 5 minutes adding to the DEVLOG
 
-Backseating the makefile (50 minutes)
+- Backseating the makefile (50 minutes)
 
-Added vector2 class
+- Added vector2 class
 
 
-### 2026-01-06 - Started on player class
 
-Worked on player.h and player.c to add playable agents.
+### 2026-01-07 - Started on player class. Added engine_clock
 
-Added Vector2 members for position and velocity as well as initialization with
-player name.
+- Worked on player.h and player.c to add playable agents.
+
+- Added Vector2 members for position and velocity as well as initialization with player name.
+
+- Added engine_clock to wait until next frame based on fps value and record deltaTime values
+
+- Added function to test frame timer and budget wasd inputs for changing player status
+
+### 2026-01-08 - 
--- src/main.c
+++ src/main.c
@@ -1,9 +1,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <ncurses.h>
+
+#include "engine_clock.h"
+#include "player.h"
 #include "ascii_image.h"
 
+void exitGame(int n){
+	curs_set(1);
+	endwin();
+	exit(n);
+}
+
+int get_wasd_input() {
+  int ch = getch();
+  if (
+    ch == 'w' || ch == 'W' || 
+    ch == 'a' || ch == 'A' || 
+    ch == 's' || ch == 'S' || 
+    ch == 'd' || ch == 'D') {
+      return ch;
+  }
+
+  return 0;
+}
+
+void testEngineClock() {
+  EngineClock_init();
+
+  Player playerStruct;
+  Player* newPlayer = &playerStruct;
+  Player_init(newPlayer, "Aleksandr");
+
+  initscr();
+  cbreak();
+  noecho();
+  nodelay(stdscr, TRUE);
+  curs_set(0);
+
+  // double timeToElapse = 1 - .001f;
+  // while (timeElapsed() < timeToElapse) {
+  while (1) {
+    double dt = waitForNextFrame();
+    char input = get_wasd_input();
+    Player_handleInput(newPlayer, input);
+    Player_updateMovement(newPlayer, dt);
+
+    clear();
+    mvprintw(0, 0, "Time Elapsed: %.2lfs", timeElapsed());
+    mvprintw(1, 0, "Pos: (%.2lf, %.2lf)", newPlayer->pos.x, newPlayer->pos.y);
+    mvprintw(2, 0, "dt=%lf | TPS=%.2f\n", dt, 1/dt);
+    mvprintw(3,0, "Current input: %c", input);
+    refresh();
+  }
+
+  exitGame(0);
+}
+
 int main(){
-	loadRawImage("assets/testsprite.txt");
-	return 0;
+	// loadRawImage("assets/testsprite.txt");
+  testEngineClock();
 }
