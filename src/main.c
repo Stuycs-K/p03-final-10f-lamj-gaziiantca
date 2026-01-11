69e2706c606967abf8fb71ca38607c94ec9bcfbe
Retrialed <jessel40@nycstudents.net>
2026-01-11 18:54:21 -0500

    Edited test event to also display player movement changes

--- src/main.c
+++ src/main.c
@@ -26,15 +26,26 @@ int get_wasd_input() {
   return 0;
 }
 
+typedef struct { 
+  int var; 
+  Vector2 lastPos;
+} MovedCtx;
+
 void testEvent(void* context, void* args) { // Check if player x pos is >= 10
-  int* editedVar = (int*) context;
+  MovedCtx* con = (MovedCtx*) context;
   Vector2 pos = *((Vector2*) args);
 
+  Vector2 delta = Vector2_sub(pos, con->lastPos);
+  con->lastPos = pos;
+
+  if (Vector2_mag(delta) > 0) {
+    mvprintw(4, 0, "Position changed by (%.3lf, %.3lf)", delta.x, delta.y);
+  }
+
   if (pos.x >= 10) {
-    *editedVar = 1; 
+    con->var = 1; 
   }
 }
-
 
 void testBudgetGameLoop() {
   EngineClock_init();
@@ -43,8 +54,8 @@ void testBudgetGameLoop() {
   Player* newPlayer = &playerStruct;
   Player_init(newPlayer, "Aleksandr");
 
-  int editedVar = 0;
-  Signal_Connect(newPlayer->moved, &testEvent, &editedVar);
+  MovedCtx* context = calloc(1, sizeof(MovedCtx));
+  Signal_Connect(newPlayer->moved, &testEvent, context);
 
   initscr();
   cbreak();
@@ -64,8 +75,8 @@ void testBudgetGameLoop() {
     mvprintw(2, 0, "dt=%lf | TPS=%.2f\n", dt, 1/dt);
     mvprintw(3, 0, "Current input: %c", input);
 
-    if (editedVar) {
-      mvprintw(4, 0, "Player x position has passed 10!!");
+    if (context->var) {
+      mvprintw(5, 0, "Player x position has passed 10!!");
     }
 
     refresh();
