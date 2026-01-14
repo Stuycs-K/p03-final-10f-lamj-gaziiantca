0241ccbc6e5a94865757b476c93836718d40fc11
agaziiantc60 <131266168+bread1337j@users.noreply.github.com>
2026-01-14 14:56:37 -0500

    mr k gonna love this one

--- src/networking.c
+++ src/networking.c
@@ -3,55 +3,55 @@
 
 // Create a socket
 int setupUDP() {
-  //setup structs for getaddrinfo//use getaddrinfo
-  struct addrinfo *results, *hints;
+	//setup structs for getaddrinfo//use getaddrinfo
+	struct addrinfo *results, *hints;
 
-  hints = (struct addrinfo*) calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET;
-  hints->ai_socktype = SOCK_DGRAM; //TCP socket
-  hints->ai_flags = AI_PASSIVE; //only needed on server
-  err(getaddrinfo(NULL, PORT, hints, &results), "getaddrinfo()");
+	hints = (struct addrinfo*) calloc(1, sizeof(struct addrinfo));
+	hints->ai_family = AF_INET;
+	hints->ai_socktype = SOCK_DGRAM; //TCP socket
+	hints->ai_flags = AI_PASSIVE; //only needed on server
+	err(getaddrinfo(NULL, PORT, hints, &results), "getaddrinfo()");
 
-  //create the socket
-  //store the socket descriptor here
-  int clientd = socket(AF_INET, SOCK_DGRAM, 0);
+	//create the socket
+	//store the socket descriptor here
+	int clientd = socket(AF_INET, SOCK_DGRAM, 0);
 
-  //this code should get around the address in use error
-  int yes = 1;
-  int sockOpt = setsockopt(clientd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-  err(sockOpt, "Sockopt Error");
+	//this code should get around the address in use error
+	int yes = 1;
+	int sockOpt = setsockopt(clientd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+	err(sockOpt, "Sockopt Error");
 
-  //bind the socket to address and port
-  err(bind(clientd, results->ai_addr, results->ai_addrlen), "Bind()");
+	//bind the socket to address and port
+	err(bind(clientd, results->ai_addr, results->ai_addrlen), "Bind()");
 
-  //free the structs used by getaddrinfo
-  free(hints);
-  freeaddrinfo(results);
+	//free the structs used by getaddrinfo
+	free(hints);
+	freeaddrinfo(results);
 
-  return clientd;
+	return clientd;
 }
 
 int setupUDP_Client(char* IP, struct addrinfo** returnedResult) {
-  struct addrinfo *results, *hints;
-  hints = (struct addrinfo*) calloc(1, sizeof(struct addrinfo));
-  hints->ai_family = AF_INET;
-  hints->ai_socktype = SOCK_DGRAM;
-  err(getaddrinfo(IP, PORT, hints, &results), "getaddrinfo()");
+	struct addrinfo *results, *hints;
+	hints = (struct addrinfo*) calloc(1, sizeof(struct addrinfo));
+	hints->ai_family = AF_INET;
+	hints->ai_socktype = SOCK_DGRAM;
+	err(getaddrinfo(IP, PORT, hints, &results), "getaddrinfo()");
 
 
-  int sockfd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
-  err(sockfd, "Socket Error");
+	int sockfd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	err(sockfd, "Socket Error");
 
-  free(hints);
+	free(hints);
 
-  *returnedResult = results;
+	*returnedResult = results;
 
-  return sockfd;
+	return sockfd;
 }
 
 void err(int i, char*message){
-  if(i < 0){
-	  printf("Error: %s - %s\n",message, strerror(errno));
-  	exit(1);
-  }
+	if(i < 0){
+		printf("Error: %s - %s\n",message, strerror(errno));
+		exit(1);
+	}
 }
